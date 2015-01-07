3f2634534cd018fdabd4f26e618ee5c8979817d6
lukasz karluk <julapy@googlemail.com>
2015-01-07 17:13:25 +1100

    ofxRandomClicker

---
+++ src/ofxRandomClicker.cpp
@@ -0,0 +1,17 @@
+//
+//  ofxRandomClicker.cpp
+//  example
+//
+//  Created by Lukasz Karluk on 7/01/2015.
+//
+//
+
+#include "ofxRandomClicker.h"
+
+ofxRandomClicker::ofxRandomClicker() {
+    //
+}
+
+ofxRandomClicker::~ofxRandomClicker() {
+    //
+}
---
+++ src/ofxRandomClicker.h
@@ -0,0 +1,18 @@
+//
+//  ofxRandomClicker.h
+//  example
+//
+//  Created by Lukasz Karluk on 7/01/2015.
+//
+//
+
+#include "ofMain.h"
+
+class ofxRandomClicker {
+    
+public:
+    
+    ofxRandomClicker();
+    ~ofxRandomClicker();
+    
+};
